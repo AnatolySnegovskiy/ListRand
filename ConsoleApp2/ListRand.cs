382ad8af10fad98754d5ed1c5a49fe63dd2a2b1d
Carrion-Crow <CarrionGrow@Gmail.com>
2018-06-19 21:03:43 +0300

    First Commit

---
+++ ConsoleApp2/ListRand.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoleApp2
+{
+    class ListNode
+    {
+        public ListNode Prev;
+        public ListNode Next;
+        public ListNode Rand; // произвольный элемент внутри списка
+        public string Data;
+    }
+
+    class ListRand
+    {
+        public ListNode Head;
+        public ListNode Tail;
+        public int Count;
+
+        public void Serialize(FileStream s)
+        {
+            Dictionary<ListNode, int> DictionaryListRand = new Dictionary<ListNode, int>();
+            ListNode temp = new ListNode();
+            int i = 0;
+            temp = Head;
+
+            do
+            {
+                DictionaryListRand.Add(temp, i);
+                temp = temp.Next;
+                i++;
+            } while (temp != null);
+
+            using (StreamWriter stringWrite = new StreamWriter(s))
+            {
+                temp = Head;
+                stringWrite.WriteLine(Count);
+
+                do
+                {
+                    stringWrite.WriteLine(temp.Data.ToString());
+                    stringWrite.WriteLine(DictionaryListRand[temp.Rand].ToString());
+                    temp = temp.Next;
+                } while (temp != null);
+            }
+        }
+
+        public void Deserialize(FileStream s)
+        {
+            Dictionary<ListNode, int> DictionaryListRand = new Dictionary<ListNode, int>();
+            ListNode[] List = null;
+            ListNode temp = new ListNode();
+            ListNode tempRandom = new ListNode();
+
+            Count = 0;
+            Head = temp;
+
+            using (StreamReader stringRead = new StreamReader(s))
+            {
+                string line;
+                int CountItems = 0;
+                int j = 0;
+
+                while (!string.IsNullOrWhiteSpace(line = stringRead.ReadLine()))
+                {
+                    if (List == null)
+                    {
+                        bool result = Int32.TryParse(line, out CountItems);
+
+                        if (result)
+                        {
+                            List = new ListNode[CountItems];
+                            continue;
+                        }
+                        else
+                        {
+                            throw new Exception("Неверный формат данных!!!");
+                        }
+                    }
+
+                    if (j % 2 == 0)
+                    {
+                        temp.Data = line;
+                        tempRandom = temp;
+                        ListNode next = new ListNode();
+                        temp.Next = next;
+                        List[Count] = temp;
+                        next.Prev = temp;
+                        temp = next;
+                        Count++;
+                    }
+                    else
+                    {
+                        int indexList;
+                        bool result = Int32.TryParse(line, out indexList);
+
+                        if (result)
+                        {
+                            DictionaryListRand.Add(temp.Prev, indexList);
+                        }
+                    }
+                    j++;
+                }
+            }
+
+            Tail = temp.Prev;
+            Tail.Next = null;
+
+            for (int i = 0; i < List.Length; i++)
+            {
+                List[i].Rand = List[DictionaryListRand[List[i]]];
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static Random rand = new Random();
+
+        static ListNode AddNode(ListNode prev)
+        {
+            ListNode result = new ListNode();
+            result.Prev = prev;
+            result.Next = null;
+            result.Data = rand.Next(0, 100).ToString();
+            prev.Next = result;
+
+            return result;
+        }
+
+        static void DisplayWrite(ListRand first)
+        {
+            ListNode temp = first.Head;
+
+            while (temp != null)
+            {
+                Console.WriteLine("[" + temp.Data.ToString() + " => Rand Data:" + temp.Rand.Data.ToString() + "]");
+                temp = temp.Next;
+            }
+        }
+
+        static int СountElement()
+        {
+            Console.Clear();
+            Console.WriteLine("Введите колличество элементов:");
+            int count;
+            bool result = Int32.TryParse(Console.ReadLine(), out count);
+
+            if (result)
+            {
+                return count;
+            }
+            else
+            {
+                return СountElement();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ListNode head = new ListNode();
+            ListNode tail = new ListNode();
+            ListNode temp = new ListNode();
+            ListRand first = new ListRand();
+            int length = СountElement();
+            ListNode[] List = new ListNode[length];
+
+            head.Data = rand.Next(0, 1000).ToString();
+            tail = head;
+            List[0] = head;
+
+            for (int i = 1; i < length; i++)
+            {
+                tail = AddNode(tail);
+                List[i] = tail;
+            }
+
+            temp = head;
+
+            for (int i = 0; i < length; i++)
+            {
+                int index = rand.Next(0, length - 1);
+                temp.Rand = List[index];
+                temp = temp.Next;
+            }
+
+            first.Head = head;
+            first.Tail = tail;
+            first.Count = length;
+
+            Console.WriteLine("Вывод созданных данных:");
+            DisplayWrite(first);
+          
+            FileStream fs = new FileStream("data.dat", FileMode.Create);
+            first.Serialize(fs);
+
+            Console.ReadLine();
+
+            ListRand second = new ListRand();
+
+            try
+            {
+                fs = new FileStream("data.dat", FileMode.Open);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Press Enter to exit.");
+                Console.Read();
+                Environment.Exit(0);
+            }
+
+            second.Deserialize(fs);
+
+            if (second.Tail.Data == first.Tail.Data)
+            {
+                Console.WriteLine("Вывод Загруженых данных данных:");
+                DisplayWrite(second);
+                Console.WriteLine("Success");
+            }
+
+            Console.Read();
+        }
+    }
+}
